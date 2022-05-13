f2963c77c1898548d64cb2b63149496a1900d072
Jimmy Provencher <jimmy.provencher@cgi.com>
2022-05-12 21:40:42 -0400

    Mute default attribute

--- attributes/chef_workstation_initialize.rb
+++ attributes/chef_workstation_initialize.rb
@@ -8,27 +8,27 @@
 # issues_url 'https://github.com/jimbodragon/infra_chef/issues'
 # source_url 'https://github.com/jimbodragon/infra_chef'
 
-default['infra_chef']['project_name'] = nil
-default['infra_chef']['chef_repo_initialized'] = false
-default['infra_chef']['chef_generator_initialized'] = false
-default['infra_chef']['project_description'] = "Build the chef infrasctructure of #{node['infra_chef']['project_name']}"
-default['infra_chef']['chef_repo_name'] = node['infra_chef']['project_name']
-default['infra_chef']['environments'] = []
-default['infra_chef']['install_dir'] = ::File.join(::Dir.pwd, "chef_repositories")
-default['infra_chef']['initial_command'] = "wget https://raw.githubusercontent.com/jimbodragon/initialize_chef_repo/master/initializator.sh && bash initializator.sh #{node['infra_chef']['project_name']} DR SIT QA Test Dev"
-default['infra_chef']['chef_solo_command'] = "chef-solo --chef-license 'accept' --json-attributes node.json --config solo.rb --override-runlist \"role[#{node['infra_chef']['project_name']}]\""
-default['infra_chef']['cron_chef_solo_command'] = "chef-solo --chef-license 'accept' --config #{::File.join("#{node['infra_chef']['install_dir']}", "solo.rb")} --override-runlist \"role[#{node['infra_chef']['project_name']}]\""
-default['infra_chef']['chef_boostrapped'] = false
-default['infra_chef']['environment'] = "production"
-default['infra_chef']['run_list'] = ["recipe[infra_chef]"]
-
-default['infra_chef']['gitinfo'] = nil
-
-default['infra_chef']['cron'] = {
-  "minute": "0,5,10,15,20,25,30,35,40,45,50,55"
-}
-
-default['chef-git-server']['compile_time'] = false
-# default['chef-git-server']['repositories'] = [ node['infra_chef']['project_name'], "#{node['infra_chef']['project_name']}_generator", "infra_chef", "initialize_chef_repo" ]
-
-default['virtualbox']['default_interface'] = "eth0"
+# default['infra_chef']['project_name'] = nil
+# default['infra_chef']['chef_repo_initialized'] = false
+# default['infra_chef']['chef_generator_initialized'] = false
+# default['infra_chef']['project_description'] = "Build the chef infrasctructure of #{node['infra_chef']['project_name']}"
+# default['infra_chef']['chef_repo_name'] = node['infra_chef']['project_name']
+# default['infra_chef']['environments'] = []
+# default['infra_chef']['install_dir'] = ::File.join(::Dir.pwd, "chef_repositories")
+# default['infra_chef']['initial_command'] = "wget https://raw.githubusercontent.com/jimbodragon/initialize_chef_repo/master/initializator.sh && bash initializator.sh #{node['infra_chef']['project_name']} DR SIT QA Test Dev"
+# default['infra_chef']['chef_solo_command'] = "chef-solo --chef-license 'accept' --json-attributes node.json --config solo.rb --override-runlist \"role[#{node['infra_chef']['project_name']}]\""
+# default['infra_chef']['cron_chef_solo_command'] = "chef-solo --chef-license 'accept' --config #{::File.join("#{node['infra_chef']['install_dir']}", "solo.rb")} --override-runlist \"role[#{node['infra_chef']['project_name']}]\""
+# default['infra_chef']['chef_boostrapped'] = false
+# default['infra_chef']['environment'] = "production"
+# default['infra_chef']['run_list'] = ["recipe[infra_chef]"]
+#
+# default['infra_chef']['gitinfo'] = nil
+#
+# default['infra_chef']['cron'] = {
+#   "minute": "0,5,10,15,20,25,30,35,40,45,50,55"
+# }
+#
+# default['chef-git-server']['compile_time'] = false
+# # default['chef-git-server']['repositories'] = [ node['infra_chef']['project_name'], "#{node['infra_chef']['project_name']}_generator", "infra_chef", "initialize_chef_repo" ]
+#
+# default['virtualbox']['default_interface'] = "eth0"
